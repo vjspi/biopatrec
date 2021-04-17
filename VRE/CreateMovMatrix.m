99ba8e13607d2570c98d2c225816d547ae0f8f40
Veronika Spieker <spieker.vj@gmail.com>
2021-04-17 11:32:26 +0200

    Resolved matrix division issue for TAC test
    matrix could not be divided by vector - resolved issue using a loop

--- VRE/CreateMovMatrix.m
+++ VRE/CreateMovMatrix.m
@@ -70,8 +70,12 @@ function [movMatrix, movMatrixNorm] = CreateMovMatrix(patRec)
             end
         end
     end
+	
+	[~, col] = size(movMatrix);
+    for i=1:col
+        movMatrixNorm(:,i) = nanmax((movMatrix(:,i) ./ sqrt(sum(movMatrix.^2,2))),0);
+    end
 
-    movMatrixNorm = movMatrix ./ sqrt(sum(movMatrix.^2,2));
     % Make sure the "Rest" norm is 0
     if restIdx > 0
         movMatrixNorm(restIdx,:) = [];
