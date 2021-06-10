bf78119a88c856e68f047be56105351d951c8a9b
Veronika Spieker <spieker.vj@gmail.com>
2021-06-10 12:23:48 +0200

    Alternative Discriminant Analysis (DA_v2) method (capable of calculating confidence intervals)

--- PatRec/OneShotPatRec.m
+++ PatRec/OneShotPatRec.m
@@ -1,14 +1,14 @@
 % ---------------------------- Copyright Notice ---------------------------
-% This file is part of BioPatRec © which is open and free software under 
+% This file is part of BioPatRec ? which is open and free software under 
 % the GNU Lesser General Public License (LGPL). See the file "LICENSE" for 
 % the full license governing this code and copyrights.
 %
 % BioPatRec was initially developed by Max J. Ortiz C. at Integrum AB and 
-% Chalmers University of Technology. All authors’ contributions must be kept
+% Chalmers University of Technology. All authors? contributions must be kept
 % acknowledged below in the section "Updates % Contributors". 
 %
 % Would you like to contribute to science and sum efforts to improve 
-% amputees’ quality of life? Join this project! or, send your comments to:
+% amputees? quality of life? Join this project! or, send your comments to:
 % maxo@chalmers.se.
 %
 % The entire copyright notice must be kept in this or any source file 
@@ -43,6 +43,11 @@ function [outMov outVector] = OneShotPatRec(patRecTrained,tSet)
     elseif strcmp(patRecTrained.algorithm,'DA')
 
         [outMov outVector] = DiscriminantTest(patRecTrained.coeff,tSet,patRecTrained.training);        
+    
+    elseif strcmp(patRecTrained.algorithm,'DA_v2')
+
+        [outMov outVector] = DiscriminantTest_v2(patRecTrained.classifier,tSet,patRecTrained.training);      
+        % outVector represesents confidence interval
 
     elseif strcmp(patRecTrained.algorithm,'RFN')     
 
