f80df046cfac61fc02bcaa9d85a89df721f59409
Veronika Spieker <spieker.vj@gmail.com>
2021-06-21 11:37:52 +0200

    Update MotionTest.m
    To include new device recording as well

--- PatRec/MotionTest.m
+++ PatRec/MotionTest.m
@@ -1,14 +1,14 @@
 % ---------------------------- Copyright Notice ---------------------------
-% This file is part of BioPatRec © which is open and free software under 
+% This file is part of BioPatRec ? which is open and free software under 
 % the GNU Lesser General Public License (LGPL). See the file "LICENSE" for 
 % the full license governing this code and copyrights.
 %
 % BioPatRec was initially developed by Max J. Ortiz C. at Integrum AB and 
-% Chalmers University of Technology. All authors’ contributions must be kept
+% Chalmers University of Technology. All authors? contributions must be kept
 % acknowledged below in the section "Updates % Contributors". 
 %
 % Would you like to contribute to science and sum efforts to improve 
-% amputees’ quality of life? Join this project! or, send your comments to:
+% amputees? quality of life? Join this project! or, send your comments to:
 % maxo@chalmers.se.
 %
 % The entire copyright notice must be kept in this or any source file 
@@ -241,7 +241,7 @@ for t = 1 : trials
 
                 % Init SBI
                 sCh = 1:nCh;
-                if strcmp(deviceName, 'Thalmic MyoBand')
+                if strcmp(deviceName, 'Thalmic MyoBand') || strcmp(deviceName, 'Thalmic MyoBand (IMU)')
                     %CK: init MyoBand
                     s = MyoBandSession(sF, sT, sCh);
                 else
@@ -261,11 +261,11 @@ for t = 1 : trials
                     s.wait();
                 end
 %                 delete(lh);
-                if ~strcmp(deviceName, 'Thalmic MyoBand')
+                if ~strcmp(deviceName, 'Thalmic MyoBand') && ~strcmp(deviceName, 'Thalmic MyoBand (IMU)')
                     delete(lh);
                 end
                 %CK: Stop sampling from MyoBand
-                if strcmp(deviceName, 'Thalmic MyoBand')
+                if strcmp(deviceName, 'Thalmic MyoBand') || strcmp(deviceName, 'Thalmic MyoBand (IMU)')
                     MyoClient('StopSampling');
                 end
                 
