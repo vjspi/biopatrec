f45c03b7071c979f15a5852091f4949509adca3b
Veronika Spieker <spieker.vj@gmail.com>
2021-07-06 18:15:58 +0200

    Adjust movement selection

--- SigRecordings/GUI_RecordingSession.m
+++ SigRecordings/GUI_RecordingSession.m
@@ -1,14 +1,14 @@
 % ---------------------------- Copyright Notice ---------------------------
-% This file is part of BioPatRec © which is open and free software under 
+% This file is part of BioPatRec ? which is open and free software under 
 % the GNU Lesser General Public License (LGPL). See the file "LICENSE" for 
 % the full license governing this code and copyrights.
 %
 % BioPatRec was initially developed by Max J. Ortiz C. at Integrum AB and 
-% Chalmers University of Technology. All authors’ contributions must be kept
+% Chalmers University of Technology. All authors? contributions must be kept
 % acknowledged below in the section "Updates % Contributors". 
 %
 % Would you like to contribute to science and sum efforts to improve 
-% amputees’ quality of life? Join this project! or, send your comments to:
+% amputees? quality of life? Join this project! or, send your comments to:
 % maxo@chalmers.se.
 %
 % The entire copyright notice must be kept in this or any source file 
@@ -21,7 +21,7 @@
 % ------------------------- Updates & Contributors ------------------------
 % [Contributors are welcome to add their email]
 % 20xx-xx-xx / Max Ortiz  / Creation
-% 20xx-xx-xx / Pontus Lövinger  / Added an alternative for ramp recording
+% 20xx-xx-xx / Pontus L?vinger  / Added an alternative for ramp recording
 % 2015-12-04 / Morten Bak Kristoffersen  / Added Arm/Leg toggle. 
 % 20xx-xx-xx / Author  / Comment on update
 
@@ -99,7 +99,7 @@ image(backgroundImage);
 axis off
 
 armMovements = {
-    'Open Hand'; 'Close Hand'; 'Flex Hand'; 'Extend Hand'; 'Pronation'; 'Supination'; 'Side Grip'; 'Fine Grip'; 'Agree'; 'Pointer'; 'Thumb Extend'; 'Thumb Flex'; 'Thumb Abduc'; 'Thumb Adduc'; 'Flex Elbow'; 'Extend Elbow'; 'Index Extend'; 'Index Flex'; 'Middle Extend'; 'Middle Flex'; 'Ring Extend'; 'Ring Flex'; 'Little Extend'; 'Little Flex';};
+    'Open Hand'; 'Close Hand'; 'Flex Hand'; 'Extend Hand'; 'Pronation'; 'Supination'; 'Pinch Grip'; 'Rest'; 'Fine Grip'; 'Agree'; 'Pointer'; 'Thumb Extend'; 'Thumb Flex'; 'Thumb Abduc'; 'Thumb Adduc'; 'Flex Elbow'; 'Extend Elbow'; 'Index Extend'; 'Index Flex'; 'Middle Extend'; 'Middle Flex'; 'Ring Extend'; 'Ring Flex'; 'Little Extend'; 'Little Flex';};
 set(handles.et_msg,'String',armMovements);
 
 %clear value of msg
